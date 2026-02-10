72e3325ef384a396eece74a6d34f7512ce88e0fa
Mason Currie <masoncu13@ymail.com>
2026-02-09 23:22:52 -0600

    Quiz app

---
+++ Slots/src/App.vue
@@ -0,0 +1,15 @@
+<script>
+  export default {
+    name: "App"
+  }
+</script>
+
+<template>
+  <form>
+    <div class="help"></div>
+    <div class="fields"></div>
+    <div class="buttons"></div>
+  </form>
+    
+</template>
+
