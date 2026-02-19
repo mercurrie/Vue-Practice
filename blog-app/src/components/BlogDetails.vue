44dfb5a624e00cd0f5d271cc893eb63b37a6cead
Mason Currie <masoncu13@ymail.com>
2026-02-18 23:30:30 -0600

    Update Read.me

--- blog-app/src/components/BlogDetails.vue
+++ blog-app/src/components/BlogDetails.vue
@@ -1,17 +1,4 @@
 <template>
-    <v-container>
-        <v-row>
-        <v-col>
-            <h1>Blog Details - ID: {{ blogId }}</h1>
-            <!-- Display blog details here -->
-        </v-col>
-        </v-row>
-    </v-container>
 </template>
-
-<script lang="ts" setup>
-
-const route = useRoute()
-const blogId = route.params.id;
 
 </script>
